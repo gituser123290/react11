8c99e84e45c8f3a650f91cb3f0c8948b15b89222
shashiweb <shashiweb64@gmail.com>
2025-03-04 11:53:54 +0530

    message of resion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Abc,{Mypage1} from './Page';
+import Myabout from './About';
+import "./style.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Abc></Abc>
+     <Mypage1></Mypage1>
+     <Myabout></Myabout>
+
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
