4ce0389f009e93759fb9edb3101a6a3d9ec9f9e2
shashiweb <shashiweb64@gmail.com>
2025-03-07 12:36:25 +0530

    chanage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
@@ -7,11 +7,13 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import Abc,{Mypage1} from './Page';
 import Myabout from './About';
 
+
 import "./style.css";
 import Mybootstrap, { Inlinecsstyle } from './Mybootstrap';
 import Myreactbootstrap from './Myreactbootstrap';
 import Mymui from './Mymui';
 import Mystate from './Mystate';
+import Mycondition from './Mycondition';
 
 
 
@@ -26,6 +28,7 @@ root.render(
      <Myreactbootstrap></Myreactbootstrap>
      <Mymui></Mymui> */}
      <Mystate></Mystate>
+     <Mycondition></Mycondition>
 
   </React.StrictMode>
 );
